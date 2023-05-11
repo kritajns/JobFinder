18e98d6a5c8770ad3b12691411fed74b5ca839c7
kritajns <kritajnshrest@gmail.com>
2023-05-11 17:19:12 +0545

    updated code

--- App/screens/LoginScreen/index.js
+++ App/screens/LoginScreen/index.js
@@ -1,14 +1,80 @@
 import React from 'react';
-import {SafeAreaView, View, Text, TouchableOpacity, Image} from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const SignInScreen = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
-      <View>
-        <Text> Welcome to Sign In!</Text>
+      <View style={styles.container}>
+        <View style={styles.headerContent}>
+          <Text style={styles.header}>UI/UX Design</Text>
+          <View style={styles.iconContainer}>
+            <Icon name="filter" size={20} style={styles.icon} />
+          </View>
+        </View>
+        <Text style={styles.miniText}>32 Job Opportunity</Text>
+        <View style={styles.tagContainer}>
+          <View style={styles.wrapper}>
+            <Text>Most Relevant</Text>
+          </View>
+          <View style={styles.wrapper}>
+            <Text>Most Recent</Text>
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 25,
+  },
+  headerContent: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 6,
+  },
+  header: {
+    fontSize: 22,
+    fontWeight: '600',
+    letterSpacing: 1,
+  },
+  iconContainer: {
+    height: 30,
+    width: 30,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+  },
+  icon: {
+    color: 'white',
+  },
+  miniText: {
+    fontSize: 12,
+    fontWeight: '400',
+  },
+  tagContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  wrapper: {
+    backgroundColor: 'grey',
+    borderRadius: 15,
+    padding: 15,
+    marginRight: 10,
+    marginVertical: 25,
+  },
+});
 
 export default SignInScreen;
