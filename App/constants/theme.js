dec55d673d52e8f96add002dfffd4ad1a70204e6
kritajns <kritajnshrest@gmail.com>
2023-05-12 17:50:31 +0545

    theme updated

--- App/constants/theme.js
+++ App/constants/theme.js
@@ -20,4 +20,32 @@ const SIZES = {
   xSmall: 10,
   small: 12,
   medium: 16,
+  large: 20,
+  xLarge: 24,
+  xxLarge: 32,
 };
+
+const SHADOWS = {
+  small: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.85,
+    elevation: 2,
+  },
+  medium: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 5.85,
+    elevation: 5,
+  },
+};
+
+export {COLORS, FONT, SIZES, SHADOWS};
