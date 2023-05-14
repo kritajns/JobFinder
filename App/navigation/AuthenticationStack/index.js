832029fc47f8bc4d08de146926f10a62b23588af
kritajns <kritajnshrest@gmail.com>
2023-05-14 08:08:35 +0545

    added homeScreen for navigaiton

--- App/navigation/AuthenticationStack/index.js
+++ App/navigation/AuthenticationStack/index.js
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import {SignInScreen, Profile} from '../../screens';
+import {HomeScreen, SignInScreen, Profile} from '../../screens';
 
 const AuthStack = createNativeStackNavigator();
 
 const AuthStackScreen = () => (
   <AuthStack.Navigator>
+    <AuthStack.Screen name="HomeScreen" component={HomeScreen} />
     <AuthStack.Screen name="LoginScreen" component={SignInScreen} />
     <AuthStack.Screen name="Profile" component={Profile} />
   </AuthStack.Navigator>
--- App/screens/index.js
+++ App/screens/index.js
@@ -1,7 +1,9 @@
+import HomeScreen from './HomeScreen';
 import SignInScreen from './LoginScreen';
 import Profile from './ProfileScreen';
 
 export {
+  HomeScreen,
   SignInScreen,
   Profile,
 };
