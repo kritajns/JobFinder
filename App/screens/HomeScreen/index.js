1e7291c5bb8de6ca46d7b20df8f5cb0dee58495e
kritajns <kritajnshrest@gmail.com>
2023-05-14 17:14:01 +0545

    updated code for home screen

--- App/screens/HomeScreen/index.js
+++ App/screens/HomeScreen/index.js
@@ -1,18 +1,29 @@
-import React from "react";
-import { SafeAreaView, ScrollView, View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
-import NearbyJobs from "../../components/home/nearby/NearbyJobs";
-import Welcome from "../../components/home/welcome/Welcome";
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+} from 'react-native';
+import NearbyJobs from '../../components/home/nearby/NearbyJobs';
+import Welcome from '../../components/home/welcome/Welcome';
 // import {NearbyJobs} from '../../components';
-import { COLORS, icons, images, SIZES } from "../../constants";
-import ScreenHeaderBtn from "../../components/common/header/ScreenHeaderBtn";
+import {COLORS, icons, images, SIZES} from '../../constants';
+import ScreenHeaderBtn from '../../components/common/header/ScreenHeaderBtn';
+import PopularJobs from '../../components/home/popular/PopularJobs';
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <ScreenHeaderBtn />
+    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}>
+      <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+      {/* <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" /> */}
       <View style={styles.container}>
-        <Text>THis is Home Screen</Text>
+        {/* <Text>THis is Home Screen</Text> */}
         <Welcome />
+        <PopularJobs />
         <NearbyJobs />
       </View>
     </SafeAreaView>
@@ -22,8 +33,9 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    padding: 25,
+    // justifyContent: 'center',
+    // alignItems: 'center',
   },
 });
 
