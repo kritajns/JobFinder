1d4d0c4f16c070e210c47a867dcdadaa88140726
kritajns <kritajnshrest@gmail.com>
2023-05-14 17:13:43 +0545

    added popularJobCard

---
+++ App/components/common/cards/popular/PopularJobCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const PopularJobCard = () => {
+  return (
+    <View>
+      <Text>Popular Job Card</Text>
+    </View>
+  );
+};
+
+export default PopularJobCard;
