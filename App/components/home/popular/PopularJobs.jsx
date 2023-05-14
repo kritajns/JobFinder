8d39a9a4928f523e4f031c941078d82bbe6b32ae
kritajns <kritajnshrest@gmail.com>
2023-05-14 17:11:11 +0545

    updated code for popularJobs

--- App/components/home/popular/PopularJobs.jsx
+++ App/components/home/popular/PopularJobs.jsx
@@ -1,10 +1,43 @@
-import React from "react";
-import {View, Text} from 'react-native';
+import React from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native';
+import styles from './popularjobs.style';
+import {COLORS, SIZES} from '../../../constants';
+import PopularJobCard from '../../common/cards/popular/PopularJobCard';
 
 const PopularJobs = () => {
+  const isLoading = false;
+  const error = false;
+
   return (
-    <View>
-      <Text>PopularJobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator />
+        ) : error ? (
+          <Text> Some Error </Text>
+        ) : (
+          <FlatList
+            data={[1, 2, 3, 4]}
+            renderItem={({item}) => <PopularJobCard item={item} />}
+            keyExtractor={item => item?.job_id}
+            contentContainerStyle={{columnGap: SIZES.medium}}
+            horizontal
+          />
+        )}
+      </View>
     </View>
   );
 };
