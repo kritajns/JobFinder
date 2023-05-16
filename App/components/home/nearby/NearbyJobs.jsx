2c29dc75583a15b1febd1dd0c66775ef0b48eb40
kritajns <kritajnshrest@gmail.com>
2023-05-17 01:38:32 +0545

    added nearby jobs & job card

--- App/components/home/nearby/NearbyJobs.jsx
+++ App/components/home/nearby/NearbyJobs.jsx
@@ -1,10 +1,43 @@
-import React from "react";
-import {View, Text} from 'react-native';
+import React from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ActivityIndicator,
+} from 'react-native';
+import styles from './nearbyjobs.style';
+import {COLORS, SIZES} from '../../../constants';
+import NearbyJobCard from '../../common/cards/nearby/NearbyJobCard';
+import useFetch from '../../../hook/useFetch';
 
 const NearbyJobs = () => {
+  const {data, isLoading, error} = useFetch
+  ('search', {
+      query: 'React developer',
+      num_pages: 1
+    })
+
   return (
-    <View>
-      <Text>NearbyJobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Nearby Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {!isLoading ? (
+          <ActivityIndicator />
+        ) : error ? (
+          <Text> Some Error </Text>
+        ) : (
+          data?.map((job) => (
+            <NearbyJobCard
+            />
+          ))
+        )}
+      </View>
     </View>
   );
 };
