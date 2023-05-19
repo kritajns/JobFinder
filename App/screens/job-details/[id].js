e5238ee7f02de6adb9320f88b49ef8de555457b0
kritajns <kritajnshrest@gmail.com>
2023-05-19 08:23:44 +0545

    added components to job details

--- App/screens/job-details/[id].js
+++ App/screens/job-details/[id].js
@@ -9,11 +9,49 @@ const JobDetails = () => {
   const Navigation = useNavigation();
   const {data, isLoading, error} = useFetch();
 
-  return (
-    <SafeAreaView>
-        <ScreenHeaderBtn />
-      <ScrollView showsVerticalScrollIndicator={false}>
+  const tabs = ['About', 'Qualifications', 'Responsibilities'];
 
+  const [refreshing, setRefreshing] = useState(false);
+  const [activeTab, setActiveTab] = useState(tabs[0]);
+
+  const onRefresh = () => {};
+
+  const displayTabContent = () => {};
+
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}>
+        <ScreenHeaderBtn
+          iconUrl={icons.left}
+          dimension="60%"
+          handlePress={() => Navigation.goBack()}
+        />
+        <ScreenHeaderBtn
+          iconUrl={icons.share}
+          dimension="60%"
+        />
+      <ScrollView showsVerticalScrollIndicator={false} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
+        {isLoading ? (
+          <ActivityIndicator size="large" color={COLORS.primary} />
+        ) : error ? (
+          <Text>Error</Text>
+        ) : data.length === 0 ? (
+          <Text>No Data</Text>
+        ) : (
+          <View style={{padding: SIZES.medium, paddingBottom: 100}}>
+            <Company
+              companyLogo={data[0].employer_logo}
+              jobTitle={data[0].job_title}
+              companyName={data[0].employer_logo}
+              Location={data[0].employer_logo}
+            />
+            <JobTabs
+              tabs={tabs}
+              activeTab={activeTab}
+              setActiveTab={setActiveTab}
+            />
+            {displayTabContent()}
+          </View>
+        )}
       </ScrollView>
     </SafeAreaView>
   )
