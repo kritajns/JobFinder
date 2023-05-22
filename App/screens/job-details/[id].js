3afb77d7a574888c365cb962d47b639446f3d284
kritajns <kritajnshrest@gmail.com>
2023-05-22 08:12:54 +0545

    added other components

--- App/screens/job-details/[id].js
+++ App/screens/job-details/[id].js
@@ -3,11 +3,21 @@ import {SafeAreaView, ScrollView, View, Text, TouchableOpacity, ActivityIndicato
 import { COLORS, icons, SIZES } from '../../constants';
 import ScreenHeaderBtn from '../../components/common/header/ScreenHeaderBtn';
 import useFetch from '../../hook/useFetch';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import Company from '../../components/jobdetails/company/Company';
+import Tabs from '../../components/jobdetails/tabs/Tabs';
+import {Specifics, JobAbout, JobFooter} from '../../components';
 
-const JobDetails = () => {
+const JobDetails = ({ jobId }) => {
   const Navigation = useNavigation();
-  const {data, isLoading, error} = useFetch();
+  const route = useRoute();
+  const params = route.params;
+  
+  const {data, isLoading, error, refetch} = useFetch('job-details', {
+    job_id: 'ps4lEfc1C2gAAAAAAAAAAA==',
+  });
+
+  console.log('params-=-==-', data);
 
   const tabs = ['About', 'Qualifications', 'Responsibilities'];
 
@@ -16,10 +26,30 @@ const JobDetails = () => {
 
   const onRefresh = () => {};
 
-  const displayTabContent = () => {};
+  const displayTabContent = () => {
+    switch (activeTab) {
+      case "Qualifications":
+        return <Specifics
+          title="Qualifications"
+          points={data[0].job_highlights?.qualifications ?? ['N/A']}
+        />
+      case "About":
+        return <JobAbout
+          info={data[0].job_description ?? ['No data provided'] }
+        />
+      case "Responsibilities":
+        return <Specifics
+          title="Responsibilities"
+          points={data[0].job_highlights?.responsibilities ?? ['N/A']}
+        />
+      default:
+        break;
+    }
+  };
 
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginHorizontal: 10}}>
         <ScreenHeaderBtn
           iconUrl={icons.left}
           dimension="60%"
@@ -29,12 +59,13 @@ const JobDetails = () => {
           iconUrl={icons.share}
           dimension="60%"
         />
+      </View>
       <ScrollView showsVerticalScrollIndicator={false} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
-        {isLoading ? (
+        {!isLoading ? (
           <ActivityIndicator size="large" color={COLORS.primary} />
         ) : error ? (
-          <Text>Error</Text>
-        ) : data.length === 0 ? (
+          <Text>Why Error</Text>
+        ) : !data || data.length === 0 ? (
           <Text>No Data</Text>
         ) : (
           <View style={{padding: SIZES.medium, paddingBottom: 100}}>
@@ -44,7 +75,7 @@ const JobDetails = () => {
               companyName={data[0].employer_logo}
               Location={data[0].employer_logo}
             />
-            <JobTabs
+            <Tabs
               tabs={tabs}
               activeTab={activeTab}
               setActiveTab={setActiveTab}
@@ -53,6 +84,8 @@ const JobDetails = () => {
           </View>
         )}
       </ScrollView>
+
+      <JobFooter url={data[0]?.job_google_link ?? 'https://careers.google.com/jobs/results'} />
     </SafeAreaView>
   )
 }
